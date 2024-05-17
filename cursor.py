b4d6696a78f8865a7a8af4403ea7670f69bda848
eliac24 <167546392+eliac24@users.noreply.github.com>
2024-05-17 11:03:49 -0400

    Add files via upload

--- cursor.py
+++ cursor.py
@@ -4,11 +4,17 @@ class Cursor:
   def __init__(self, x ,y ):
     self.x = x
     self.y = y
-    self.image = pygame.image.load("cursor.png")
+    self.image = pygame.image.load("CursoR.png")
+    self.rescale_image(self.image)
     self.image_size= self.image.get_size()
-    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+    self.rect = pygame.Rect(self.x, self.y, self.image_size[0] * .3, self.image_size[0] *.3)
 
+
+  def rescale_image(self,image):
+    self.image_size = self.image.get_size()
+    scale_size= (self.image_size[0] * .2 , self.image_size[1] * .2 )
+    self.image = pygame.transform.scale(self.image, scale_size)
   def move(self,new_x,new_y):
     self.x = new_x
     self.y = new_y
-    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+    self.rect = pygame.Rect(self.x, self.y, self.image_size[0] *.5, self.image_size[0] * .5)
--- main.py
+++ main.py
@@ -6,11 +6,22 @@ from blue import Blue
 from green import Green
 from purple import Purple
 from pink import Pink
-import time 
+import time
+
+
+
+#time
+start_time= time.time()
+time_1= float(10)
+time_2= float(7)
+time_3= float(5)
+time_4= float(3)
+time_5= float(2)
+
 
 #basic booleans
 click_start = False
-run= False 
+run= True 
 level_1 = False
 level_2 = False
 level_3 = False
@@ -23,46 +34,83 @@ color_gems= ["red", "blue", "green", "purple", "pink"]
 
 pygame.init()
 pygame.font.init()
-my_font = pygame.font.SysFont('Arial', 25)
+my_font = pygame.font.SysFont('Arial', 15)
 pygame.display.set_caption("Quick Thinking!")
 
 #intro messages
 introduction= my_font.render("Welcome to Quick thinking", True, (0,0,0))
-directions1 = my_font.render("In this game, the background will show (in text) a color you have to collect.You need to pick the right colored gem in a certain time", True, (0,0,0))
+directions1 = my_font.render("In this game, the background will show (in text) a color you have to collect", True, (0,0,0))
 directions2 = my_font.render("You need to quickly pick the right gem to advance to the next level", True, (0,0,0))
-lose_directions = my_font.render("If you are not quick enough you have to repeat the level",True, (0,0,0))
+
+game_start= my_font.render("click if you are ready to start", True, (0,0,0))
 
 
-r= 255
-g= 250
-b= 70
+
 
 
 #sprites
 c= Cursor(200,150)
-r= Red(random.randint(0,300),random.randint(0,300))
-p= Pink(random.randint(0,300), random.randint(0,300))
-g= Green(random.randint(0,300), random.randint(0,300))
-b= Blue(random.randint(0,300), random.randint(0,300))
-pu= Purple(random.randint(0,300), random.randint(0,300))
-        
+red= Red(random.randint(0,300),random.randint(0,300))
+pink= Pink(random.randint(0,300), random.randint(0,300))
+green= Green(random.randint(0,300), random.randint(0,300))
+blue = Blue(random.randint(0,300), random.randint(0,300))
+purple= Purple(random.randint(0,300), random.randint(0,300))
+level= 0
 
 # set up variables for the display
 SCREEN_HEIGHT = 370
 SCREEN_WIDTH = 530
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
+r= 100
+g= 10
+b= 90
 
 
-#Main game loop 
-while run:
-  for event in pygame.event.get():
-   if event.type == pygame.QUIT:
-    run= False 
-  
+while run :
+   # --- Main event loop
+   ## ----- NO BLIT ZONE START ----- ##
+   for event in pygame.event.get():
+       # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            click_start= True
+            level += 1
+        if level ==1:
+            level_1_border= my_font.render("Level One: You have 10 seconds", True, (0,0,0))
+            level_1 == True
+        if level == 2:
+            level_2_border = my_font.render("level two: You have 8 seconds", True, (0,0,0))
+            level_2== True
+        if level == 3:
+            level_3_border = my_font.render("Level three: You have 6 seconds", True, (0,0,0))
+            level_3 == True
+        if level == 4:
+            level_4_border = my_font.render("Level four: You have 4 seconds", True, (0,0,0))
+            level_4 == True
 
-screen.fill(r,g,b)
-if click_start == False:
-   screen.blit(introduction, (150,150))
-   screen.blit(directions1, (150,200))
-   screen.blit(directions2, (150,250))
+   #blitting station#
+   screen.fill((r,g,b))
+
+
+
+
+
+   if click_start == False:
+    screen.blit(introduction,(5,100))
+    screen.blit(directions1,(5,150))
+    screen.blit(directions2,(5,200))
+
+    screen.blit(game_start, (5,300))
+
+
+   if click_start == True:
+       screen.blit(c.image, c.rect)
+
+   if click_start== True and level_1 == True:
+
+   pygame.display.update()
+
+
+pygame.quit()
